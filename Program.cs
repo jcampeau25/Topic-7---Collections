453f21fe0194a88b278666e923419f25f937cd63
Joshua Campeau <339729675@GoTVDSB.ca>
2026-03-03 13:02:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Topic_7___Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random generator = new Random();
+
+        List <int> ints = new List <int> (); 
+
+        for (int i = 1; i >= 25; i++)
+            {
+                ints.Add(generator.Next(10, 21));
+            }
+
+            Console.WriteLine(ints[7]);
+
+
+        }
+    }
+}
